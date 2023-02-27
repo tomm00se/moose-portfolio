4465f4b72f6904460967de7a4db76a852b32a4fe
Moose17 <63780114+Mooose17@users.noreply.github.com>
2023-02-27 16:09:00 +0000

    made navbar layout with profile and boxshadow css

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,34 @@
+import Styles from "../components/Layout.module.css";
+
 const Layout = () => {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className={Styles.layout}>
+      <div className={Styles.header__container}>
+        <div className={Styles.header__name}>
+          <h1>Moose.</h1>
+        </div>
+        <div className={Styles.navbar__container}>
+          <ul className={Styles.navbar__parent}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={Styles.empty}></div>
+      </div>
+      <div className={Styles.profile__container}>
+        <img
+          className={Styles.profile__image}
+          src="/tommy_bowden.png"
+          alt="image of me"
+        />
+        <div className={Styles.profile__textblock}>
+          <h1 className={Styles.profile__title}>Tommy Bowden</h1>
+          <p className={Styles.profile__tagline}>Software Engineer @ xDesign</p>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from "@/components/layout";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
